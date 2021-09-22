5eab1e3c9fa88cefe36f519d8f2fa4a4bbeebec6
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-09-21 19:42:37 -0500

    fix deployment

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,13 +11,13 @@ class Nav extends React.Component {
       <nav className={this.props.class}>
         <h1 className="logo">Math Magicians</h1>
         <ul className='menu-container'>
-          <Link to="https://felipeg005.github.io/Math-Magicians-React-App/" style={linkStyles}>
+          <Link to="/Math-Magicians-React-App/" style={linkStyles}>
             <li>Home</li>
           </Link>
-          <Link to="https://felipeg005.github.io/Math-Magicians-React-App/calculator" style={linkStyles}>
+          <Link to="/calculator" style={linkStyles}>
             <li>Calculator</li>
           </Link>
-          <Link to="https://felipeg005.github.io/Math-Magicians-React-App/quote" style={linkStyles}>
+          <Link to="/quote" style={linkStyles}>
             <li>Quote</li>
           </Link>
         </ul>
